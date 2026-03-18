9d57b7dd0acdcc1ba5d985e5f2c3e3382df13af2
unknown <er.a.khan47@gmail.com>
2026-03-19 03:48:37 +0530

    template has been created

---
+++ template.py
@@ -0,0 +1,38 @@
+import os
+
+folders = [
+    "src",
+    "src/models",
+    "src/schemas",
+    "src/crud",
+    "src/api",
+    "src/core"
+]
+
+files = [
+    "src/main.py",
+    "src/database.py",
+    "src/models/address.py",
+    "src/schemas/address.py",
+    "src/crud/address.py",
+    "src/api/address.py",
+    "src/core/logger.py",
+    "src/core/exceptions.py",
+]
+
+# Create folders
+for folder in folders:
+    os.makedirs(folder, exist_ok=True)
+
+# Create files
+for file in files:
+    with open(file, "w") as f:
+        pass
+
+# Create __init__.py 
+for folder in folders:
+    init_file = os.path.join(folder, "__init__.py")
+    with open(init_file, "w") as f:
+        pass
+
+print("FastAPI project structure created!")
